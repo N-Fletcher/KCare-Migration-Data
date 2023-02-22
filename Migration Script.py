d688ff139ae03929e6bd63541a6e45ae57469ccc
Nathan Fletcher <natfletcher03@gmail.com>
2023-02-22 10:37:16 -0800

    Minor code clean-up

--- Migration Script.py
+++ Migration Script.py
@@ -1,12 +1,15 @@
-import csv, pandas as pd
+import csv
 
-#Enter file and agency name here
+''' Enter file and agency name here - this is the only manual input needed -
+Any changes to main data file name or spreadsheet formatting require the code to be updated
+'''
 input_file = 'CSKF Final.csv'
 agency = 'Creative Solutions for Kids & Families'
 
 
-#Function cleans up 'Took' column so that all values can be read as ints
-def formatTime(value) -> int:
+'''External Functions'''
+#Function cleans up 'Took' column so that all values can be read as integers
+def formatTime(value) -> float:
 
     if value.strip() == "<1 minute":
         return 0.5
@@ -16,6 +19,7 @@ def formatTime(value) -> int:
 #Function calculates updated, new and migrated values, accounting for commas
 def calculate(updated, new) -> tuple[int, int, int]:
 
+    #If statements account for values with commas (values in the thousands)
     if updated.__contains__(','):
         updated = int(row[5].strip().replace(',', ''))
     else: updated = int(row[5])
@@ -32,16 +36,16 @@ def calculate(updated, new) -> tuple[int, int, int]:
 def appendRow(agency, date, system, category, document, total, new, 
               updated, migrated, skipped, time, speed):
     
-    # with open("Migration Data.csv", "a") as all_data:
-    #     writer = csv.writer(all_data)
-    with open("Migration Data.csv", "a", newline='') as all_data:
+    #'a' = append, rather than write which would wipe the file every time
+    #by default, csv tool adds an extra line after every row, so 'newline = ""' disbales this
+    with open("Migration Data.csv", "a", newline= "") as all_data:
         writer = csv.writer(all_data)
         writer.writerow([agency, date, system, category, document, total, new, 
                          updated, migrated, skipped, time, speed])
         
 
 
-#Main
+'''Main function'''
 with open(input_file, 'r') as input:
     reader = csv.reader(input)
     date = ''
@@ -57,10 +61,13 @@ with open(input_file, 'r') as input:
         #Constantly checks for header box that contains all of the migration info,
         #looks for 'FC' and 'GCM' keywords within that box
         if row[0].lower().__contains__('(fc'): system = 'FC'
+                                    #script finds unintended 'fc' characters, '(' combats this
         elif row[0].lower().__contains__('gcm'): system = 'GCM'
 
         #Checks to see whether row contains data first, then runs transfer code
         if (row[2] != '') & (row[8].__contains__('minute') | row[8].strip().isdigit()):
+            #Column mappings specifically outlined to avoid headaches 
+            #later on should formatting change at all
             document = row[2]
             category = row[0]
             total = row[4]
@@ -69,5 +76,6 @@ with open(input_file, 'r') as input:
             time = formatTime(row[8])
             speed = float(migrated) / float(time)
 
+            #writing function externalized to avoid quadruple nesting
             appendRow(agency, date, system, category, document, total, new,
                       updated, migrated, skipped, time, speed)
